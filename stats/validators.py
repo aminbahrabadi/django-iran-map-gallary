f89274bb93810cb6eccc1de1b67f2ae80704aad7
Amin Bahrabadi <amin.bfamily@gmail.com>
2022-01-30 21:20:31 +0330

    Stats: Optimize image validator

--- stats/validators.py
+++ stats/validators.py
@@ -5,6 +5,6 @@ def image_field_validator(image):
     allowed_extensions = ['jpg', 'png', 'jpeg']
     image_extension = image.name.split('.')[-1]
 
-    if image_extension not in allowed_extensions:
+    if image_extension.lower() not in allowed_extensions:
         raise ValidationError('لطفا تصویر با '
                               'فرمت {} وارد کنید'.format(' | '.join(allowed_extensions)))
